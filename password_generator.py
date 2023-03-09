888d3f0a2e8a4b56e0dbead055611fa09920de01
gopikaks <gopikaks3568@gmail.com>
2023-03-09 20:29:00 +0530

    Password Generator uploaded

---
+++ password_generator.py
@@ -0,0 +1,29 @@
+
+#Password Generator Project
+import random as r
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password=''
+for i in range(nr_letters):
+  no_ran_letter=r.randint(0,len(letters))
+  password= password+ letters[no_ran_letter-1]
+
+for i in range(nr_symbols):
+  no_ran_symbols=r.randint(0,len(symbols))
+  password = password+ symbols[no_ran_symbols-1]
+
+for i in range(nr_numbers):
+  no_ran_numbers=r.randint(0,len(numbers))
+  password=password+ numbers[no_ran_numbers-1]
+print(password)
+  
